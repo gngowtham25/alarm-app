336869ca2e30ebabe7e74998f89388bef94bb445
Gowtham Gn <Gowtham@Gowthams-iMac.local>
2017-08-18 09:16:39 +0530

    Added an animation while opening window

--- src/app/alarm/alarm.component.ts
+++ src/app/alarm/alarm.component.ts
@@ -3,15 +3,25 @@ import { FormsModule, FormControl } from '@angular/forms';
 import { AlarmModel } from './alarm.model';
 import { ModalDirective, ComponentsHelper } from '../../../node_modules/ng2-bootstrap/ng2-bootstrap';
 import { Observable } from 'rxjs/Rx';
+import { trigger, state, style, animate, transition } from '../../../node_modules/@angular/core';
+
 
 
 
 @Component({
   selector: 'alarm',
   templateUrl: './alarm.component.html',
-  styleUrls: ['./alarm.component.css']
+  styleUrls: ['./alarm.component.css'],
+  animations:[
+    trigger('openingAnimation', [
+      state('shown', style({ opacity: 1 })),
+      state('hidden', style({ opacity: 0 })),
+      transition('shown => hidden', animate('.5s')),
+      transition('hidden => shown', animate('.6s'))
+    ])
+  ]
 })
-export class AlarmComponent implements OnInit {
+export class AlarmComponent implements OnInit,AfterViewInit {
 
   title = 'Alarm';
 
@@ -22,12 +32,15 @@ export class AlarmComponent implements OnInit {
   time: string;
   newTime: any;
   validCheck: boolean;
-  alarmId:number;
-  currentAlarm : AlarmModel;
+  alarmId: number;
+  currentAlarm: AlarmModel;
+  alarmIndex: number;
+  animateOpen : string="hidden";
 
   @ViewChild('informationModal') public informationModal: ModalDirective;
   @ViewChild('alarmModal') public alarmModal: ModalDirective;
-  
+  @ViewChild('deleteModal') public deleteModal: ModalDirective;
+
 
   ngOnInit() {
     this.getAlarms();
@@ -37,11 +50,15 @@ export class AlarmComponent implements OnInit {
     });
   }
 
+  ngAfterViewInit(){
+    this.animateOpen = 'shown';
+  }
+
   getAlarms() {
     var alarm = localStorage.getItem("alarm");
     if (!(alarm === 'undefined' || alarm.length == 0)) {
       this.alarmModel = JSON.parse(alarm);
-      this.alarmModel.filter((eachAlarm : AlarmModel)=>{
+      this.alarmModel.filter((eachAlarm: AlarmModel) => {
         eachAlarm.displayTime = this.getDisplayTime(eachAlarm);
       })
     }
@@ -50,12 +67,12 @@ export class AlarmComponent implements OnInit {
 
   showAlarmDetails(selectedAlarm: AlarmModel, operation: string) {
 
-    if(operation==='add'){
+    if (operation === 'add') {
       this.alarmName = "";
       this.repeat = false;
       this.status = true;
       this.time = new Date().toString();
-      this.alarmId=this.generateAlarmId()+1;
+      this.alarmId = this.generateAlarmId() + 1;
     }
 
     if (operation === 'edit') {
@@ -63,7 +80,7 @@ export class AlarmComponent implements OnInit {
       this.repeat = selectedAlarm.repeat;
       this.status = selectedAlarm.status;
       this.time = selectedAlarm.time;
-      this.alarmId=selectedAlarm.id;
+      this.alarmId = selectedAlarm.id;
     }
     this.informationModal.show();
 
@@ -71,18 +88,18 @@ export class AlarmComponent implements OnInit {
 
   addAlarm() {
 
-    var check: boolean = false;   
+    var check: boolean = false;
 
-    if(this.time === undefined || this.time === null){
-        return;
+    if (this.time === undefined || this.time === null) {
+      return;
     }
 
-    if(this.alarmName.trim().length<1){
+    if (this.alarmName.trim().length < 1) {
       this.alarmName = this.generateAlarmName(this.time);
     }
 
-     var obj: AlarmModel = { "title": this.alarmName, "repeat": this.repeat, "status": this.status, "time": this.time,"id":this.alarmId,"displayTime":"",tickStatus:false };
-     obj.displayTime = this.getDisplayTime(obj);
+    var obj: AlarmModel = { "title": this.alarmName, "repeat": this.repeat, "status": this.status, "time": this.time, "id": this.alarmId, "displayTime": "", tickStatus: false };
+    obj.displayTime = this.getDisplayTime(obj);
 
     if (this.time != undefined) {
       this.alarmModel.filter((eachAlarm) => {
@@ -90,7 +107,7 @@ export class AlarmComponent implements OnInit {
           eachAlarm.status = this.status;
           eachAlarm.repeat = this.repeat;
           eachAlarm.time = this.time;
-          eachAlarm.title=this.alarmName;
+          eachAlarm.title = this.alarmName;
           eachAlarm.displayTime = this.getDisplayTime(eachAlarm);
           check = true;
         }
@@ -109,66 +126,74 @@ export class AlarmComponent implements OnInit {
     if (this.alarmModel != undefined && this.alarmModel.length > 0) {
 
       var date = new Date();
-      var currentTime = date.getHours() + "" + date.getMinutes()+ "" + date.getSeconds();
+      var currentTime = date.getHours() + "" + date.getMinutes() + "" + date.getSeconds();
       this.alarmModel.filter((eachAlarm: AlarmModel) => {
         if (eachAlarm.status) {
           var alarmDate = new Date(eachAlarm.time);
-          var alarmTime = alarmDate.getHours() + "" + alarmDate.getMinutes() + "0";          
+          var alarmTime = alarmDate.getHours() + "" + alarmDate.getMinutes() + "0";
           if (alarmTime == currentTime) {
-            if(!eachAlarm.repeat){
+            if (!eachAlarm.repeat) {
               eachAlarm.status = false;
             }
-            this.currentAlarm = eachAlarm;     
+            this.currentAlarm = eachAlarm;
             this.alarmModal.show();
-          } 
-          localStorage.setItem("alarm",JSON.stringify(this.alarmModel));
+          }
+          localStorage.setItem("alarm", JSON.stringify(this.alarmModel));
         }
-    })
+      })
+    }
   }
-}
 
-getDisplayTime(alarm : AlarmModel){
-   var eachAlarmDate = new Date(alarm.time);
-        var meridian = (eachAlarmDate.getHours()>=12) ? "PM" : "AM"; 
-        var twelvehour = (eachAlarmDate.getHours()>12) ? eachAlarmDate.getHours()-12 : eachAlarmDate.getHours();
-        var hour = (twelvehour.toString() == "0")? "12": twelvehour; 
-        var hours = (hour.toString().length>1)?hour:"0"+hour;
-        var minutes = (eachAlarmDate.getMinutes().toString().length>1)?eachAlarmDate.getMinutes():"0"+eachAlarmDate.getMinutes();
-        return hours + " : " + minutes + " " + meridian;  
+  getDisplayTime(alarm: AlarmModel) {
+    var eachAlarmDate = new Date(alarm.time);
+    var meridian = (eachAlarmDate.getHours() >= 12) ? "PM" : "AM";
+    var twelvehour = (eachAlarmDate.getHours() > 12) ? eachAlarmDate.getHours() - 12 : eachAlarmDate.getHours();
+    var hour = (twelvehour.toString() == "0") ? "12" : twelvehour;
+    var hours = (hour.toString().length > 1) ? hour : "0" + hour;
+    var minutes = (eachAlarmDate.getMinutes().toString().length > 1) ? eachAlarmDate.getMinutes() : "0" + eachAlarmDate.getMinutes();
+    return hours + " : " + minutes + " " + meridian;
 
-}
-
-windowCloseFun(){
-  this.informationModal.hide();
-}
-
-closeAlarm(){
-  this.alarmModal.hide();
-  
-}
-
-generateAlarmName(time : string){
-  var date = new Date(time);
-  if(date.getHours()<6){
-    return 'Morning Alarm';
-  } else if(date.getHours()<12){
-    return 'Forenoon Alarm';
-  } else if(date.getHours()<18){
-    return 'Postnoon Alarm';
-  } else {
-    return 'Night Alarm';
   }
-}
 
-deleteAlarm(alarm : AlarmModel){
-  var index = this.alarmModel.indexOf(alarm);
-  this.alarmModel.splice(index,1);
-  localStorage.setItem("alarm",JSON.stringify(this.alarmModel));
-}
+  windowCloseFun() {
+    this.informationModal.hide();
+  }
 
-generateAlarmId(){
-  return Math.max.apply(Math,this.alarmModel.map(function(o){return o.id;}))
-}
+  closeAlarm() {
+    this.alarmModal.hide();
 
+  }
+
+  generateAlarmName(time: string) {
+    var date = new Date(time);
+    if (date.getHours() < 6) {
+      return 'Morning Alarm';
+    } else if (date.getHours() < 12) {
+      return 'Forenoon Alarm';
+    } else if (date.getHours() < 18) {
+      return 'Postnoon Alarm';
+    } else {
+      return 'Night Alarm';
+    }
+  }
+
+  deleteAlarm(alarm: AlarmModel) {
+    this.alarmIndex = this.alarmModel.indexOf(alarm);
+    this.deleteModal.show();
+
+  }
+
+  generateAlarmId() {
+    return Math.max.apply(Math, this.alarmModel.map(function (eachAlarm) { return eachAlarm.id; }))
+  }
+  deletethisAlarm() {
+    this.alarmModel.splice(this.alarmIndex, 1);
+    localStorage.setItem("alarm", JSON.stringify(this.alarmModel));
+    this.deleteModal.hide();
+  }
+  closeDelete() {
+    this.deleteModal.hide();
+    this.alarmIndex = null;
+  }
 }
 
